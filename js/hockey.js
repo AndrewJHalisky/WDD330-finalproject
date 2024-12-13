45b048fb91b2b79e80e5ff0e5cc492a3071deeac
AndrewJHalisky <ajhalisky@gmail.com>
2024-12-13 11:13:14 -0500

    Got APIs to work

--- js/hockey.js
+++ js/hockey.js
@@ -1,38 +1,40 @@
-// Function to get the URL
 const options = {
 	method: 'GET',
 	headers: {
 		'x-rapidapi-key': '472ac25993msha03284bcae70e16p15acddjsn2fb3e993030c',
-		'x-rapidapi-host': 'nhl-ncaah-api.p.rapidapi.com'
+		'x-rapidapi-host': 'hockey-highlights-api.p.rapidapi.com'
 	}
 };
+
 console.log('hello');
 let getTeamsBtn = document.getElementById("getTeams");
 getTeamsBtn.addEventListener('click', getNHLTeams);
 
 function getNHLTeams() {
-    fetch(`https://nhl-ncaah-api.p.rapidapi.com/teams/`, options)
-        .then(response => {return response.json()})
-        .then(data => {
-            data.forEach(team => {
-                const html = `<img id=${team.id} src=${team.logo} alt="teamLogo"/>
-                <h2>${team.displayName}</h2>
-                <p>Abbreviation: ${team.abbreviation}</p>
-                <p>League: ${team.league}</p>`
-                document.getElementById('showTeams').insertAdjacentHTML('beforeend', html)
-                let teamImg = document.getElementById(team.id);
-                teamImg.addEventListener('click', showDetails)
-                })})
-        .catch(error => console.log(error))
+    // Show Teams
+    fetch(`https://hockey-highlights-api.p.rapidapi.com/teams`, options)
+    .then(response => {return response.json()})
+    .then(data => {
+        for (let team of data.data) {
+            console.log(team);
+            const html = `<img id=${team.id} src="${team.logo}" alt="logo">
+            <h2>Team: ${team.name}</h2>`
+            document.getElementById('showTeams').insertAdjacentHTML('beforeend', html)
+            let teams = document.getElementById(team.id);
+            teams.addEventListener('click', showDetails)
+        }})
+    .catch(error => console.log(error))
 }
 
 function showDetails(e) {
-    let id = e.currentTarget.id;
-    fetch(`https://nhl-ncaah-api.p.rapidapi.com/teams/${id}`, options)
+    let id = e.currentTarget.teams;
+    fetch(`https://hockey-highlights-api.p.rapidapi.com/teams/${id}`, options)
     .then(response => {return response.json()})
     .then(data => {
-        data.forEach(team => {
-        })
-    })
-
+        for (let team of data.data) {
+            const html = `<img src="${team.logo}" alt="logo">
+            <h2>Team: ${team.name}</h2>`
+            document.getElementById('showTeams').insertAdjacentHTML('beforeend', html);
+        }})
+    .catch(error => console.log(error))
 }
