fd091ae6b1e6b5765ccd58c5d575e0074a469d78
AndrewJHalisky <ajhalisky@gmail.com>
2024-12-18 00:00:19 -0500

    Made a teamId comment

--- js/hockey.js
+++ js/hockey.js
@@ -1,4 +1,6 @@
 teamDetails = []
+// const teamId 
+
 const options = {
 	method: 'GET',
 	headers: {
@@ -16,7 +18,7 @@ function getNHLTeams() {
     .then(response => {return response.json()})
     .then(data => {
         for (let team of data.data) {
-            console.log(team);
+            console.log(team.id);
             const html = `<img id=${team.id} src="${team.logo}" alt="logo">
             <h2>Team: ${team.name}</h2>
             <p>ID: ${team.id} </p>`
@@ -24,45 +26,21 @@ function getNHLTeams() {
             let teamLogo = document.getElementById(team.id);
             teamLogo.addEventListener('click', () => showDetails(team.id))
             // Nested function: 
-            function showDetails(teamId) {
-                fetch(`https://hockey-highlights-api.p.rapidapi.com/matches/${team.id}`, options)
-                    .then(response => { return response.json()})
-                    .then(teamDetails => {
-                        // Check if the response contains a valid `data` property
-                        console.log("Team Details for ID", teamId, ":", teamDetails);
-                        if (teamDetails.length >= 0) {
-                            const team = teamDetails
-                            const html = `<img id=${team[0].id} src="${team[0].logo}" alt="logo">
-                            <h2>Team: ${team[0].name}</h2>
-                            <h3>${team[0].homeTeam}</h3>
-                            <h3>vs ${team[0].awayTeam}</h3>
-                            <p>City: ${team[0].city}</p>
-                            <p>Week: ${team[0].week}</p>
-                            <p>Date: ${team[0].date}</p>`;
-                            console.log(team[0])
-                            document.getElementById('getDetails').insertAdjacentHTML('beforeend', html);
-                        } else {
-                            console.error("Unexpected API response:", teamDetails[0]);
-                        }
-                    })
-                    .catch(error => console.error(error));
-            }
         }})
     .catch(error => console.log(error))
 }
 
 function showDetails(teamId) {
-    fetch(`https://hockey-highlights-api.p.rapidapi.com/matches/${teamId}`, options)
+    fetch(`https://hockey-highlights-api.p.rapidapi.com/matches/homeTeamId=${teamId}`, options)
         .then(response => { return response.json()})
         .then(teamDetails => {
             // Check if the response contains a valid `data` property
             console.log("Team Details for ID", teamId, ":", teamDetails);
             if (teamDetails.length >= 0) {
                 const team = teamDetails
-                const html = `<img id=${team[0].id} src="${team[0].logo}" alt="logo">
+                const html = `<img id=${team[0].homeTeam.id} src="${team[0].logo}" alt="logo">
                 <h2>Team: ${team[0].name}</h2>
                 <h3>${team[0].homeTeam}</h3>
-                <h3>vs ${team[0].awayTeam}</h3>
                 <p>City: ${team[0].city}</p>
                 <p>Week: ${team[0].week}</p>
                 <p>Date: ${team[0].date}</p>`;
